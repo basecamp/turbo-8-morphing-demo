944ad5dce0b3affdb4e4994fc3609b4393ffa7d5
Jorge Manrubia <jorge@hey.com>
2023-11-26 19:08:53 +0100

    Extract partials and add ids we'll need for the stream actions approach

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,6 @@
 module ProjectsHelper
   def project_progress_bar_for(project)
-    tag.progress "#{project.completion_ratio * 100}%", value: project.tasks.completed.count, max: project.tasks.count, class: "progress is-primary"
+    tag.progress "#{project.completion_ratio * 100}%", id: dom_id(project, :progress_bar),
+      value: project.tasks.completed.count, max: project.tasks.count, class: "progress is-primary"
   end
 end
