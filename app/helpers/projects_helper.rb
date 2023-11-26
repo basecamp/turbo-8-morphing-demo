1593255f34424c4a01000e757d742b0ddee3f843
Jorge Manrubia <jorge@hey.com>
2023-11-26 09:01:47 +0100

    Render tasks

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,5 @@
 module ProjectsHelper
+  def project_progress_bar_for(project)
+    tag.progress "#{project.completion_ratio * 100}%", value: project.tasks.completed.count, max: project.tasks.count, class: "progress is-primary"
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,11 @@
 class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
+
+  def completion_ratio
+    if tasks.count.any?
+      tasks.completed.count.to_f / tasks.count
+    else
+      0
+    end
+  end
 end
