836744453ca18b016843022105f299feb7c12824
Jorge Manrubia <jorge@hey.com>
2023-11-26 09:46:42 +0100

    Place tasks inside projects

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,69 +1,54 @@
 class TasksController < ApplicationController
+  before_action :set_project
   before_action :set_task, only: %i[ show edit update destroy ]
 
-  # GET /tasks or /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = @project.tasks
   end
 
-  # GET /tasks/1 or /tasks/1.json
   def show
   end
 
-  # GET /tasks/new
   def new
-    @task = Task.new
+    @task = @project.tasks.build
   end
 
-  # GET /tasks/1/edit
   def edit
   end
 
-  # POST /tasks or /tasks.json
   def create
-    @task = Task.new(task_params)
+    @task = @project.tasks.build task_params
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_to project_url(@project), notice: "Task was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /tasks/1 or /tasks/1.json
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to task_url(@task), notice: "Task was successfully updated." }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      redirect_to project_url(@project), notice: "Task was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /tasks/1 or /tasks/1.json
   def destroy
     @task.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to tasks_url, notice: "Task was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
+    def set_project
+      @project = Project.find(params[:project_id])
     end
 
-    # Only allow a list of trusted parameters through.
+    def set_task
+      @task = @project.tasks.find(params[:id])
+    end
+
     def task_params
       params.require(:task).permit(:title, :completed, :project_id)
     end
