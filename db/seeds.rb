90840b3422fbc2405acce7be721d573d4021fc55
Jorge Manrubia <jorge@hey.com>
2023-11-26 17:56:59 +0100

    Tweak seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-PROJECT_COUNT = 30
-TASKS_PER_PROJECT_COUNT = 20
+PROJECT_COUNT = 10
+TASKS_PER_PROJECT_COUNT = 6
 
 PROJECT_COUNT.times.each do |project_index|
-  project = Project.create!(name: "Project #{project_index}")
+  project = Project.create!(name: "Project #{project_index + 1}")
 
   TASKS_PER_PROJECT_COUNT.times.each do |task_index|
-    project.tasks.create!(title: "Task #{task_index}")
+    project.tasks.create!(title: "Task #{task_index + 1}")
   end
 end
