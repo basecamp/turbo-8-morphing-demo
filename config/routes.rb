dbdea920501afba7d1848ea3813a6e34b5458a63
Jorge Manrubia <jorge@hey.com>
2023-11-26 09:46:42 +0100

    Place tasks inside projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :projects
+  resources :projects do
+    resources :tasks
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
