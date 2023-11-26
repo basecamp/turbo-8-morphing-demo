9e6f3af5dbb1122dad945f00450d9b2be2dfb8e9
Jorge Manrubia <jorge@hey.com>
2023-11-26 17:47:29 +0100

    Rename local var

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 PROJECT_COUNT = 30
 TASKS_PER_PROJECT_COUNT = 20
 
-PROJECT_COUNT.times.each do |project_id|
-  project = Project.create!(name: "Project #{project_id}")
+PROJECT_COUNT.times.each do |project_index|
+  project = Project.create!(name: "Project #{project_index}")
 
-  TASKS_PER_PROJECT_COUNT.times.each do |task_id|
-    project.tasks.create!(title: "Task #{task_id}")
+  TASKS_PER_PROJECT_COUNT.times.each do |task_index|
+    project.tasks.create!(title: "Task #{task_index}")
   end
 end
