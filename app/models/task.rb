3ada4001a0aa6280f68e57b8fc749583516445ad
Jorge Manrubia <jorge@hey.com>
2023-11-27 09:04:20 +0100

    Broadcasts

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,11 @@ class Task < ApplicationRecord
   scope :pending, -> { where(completed: false) }
 
   validates :title, presence: true
+
+  after_update_commit :broadcast_update_later
+
+  private
+    def broadcast_update_later
+      broadcast_render_later_to project, partial: "tasks/update", locals: { task: self, project: project }
+    end
 end
